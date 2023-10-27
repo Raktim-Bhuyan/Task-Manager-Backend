207cca57fc42e8a0fd06520ccda4263b143aedf8
Raktim Bhuyan <rokuongo@gmail.com>
2023-10-27 22:34:27 +0530

    configure mongo connection string

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
+const { options } = require("./routes/auth");
 require("dotenv").config();
 
-const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@taskman.grnbfob.mongodb.net/?retryWrites=true&w=majority`;
+const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@taskman.grnbfob.mongodb.net/db/?retryWrites=true&w=majority`;
 
 const connectToMongo = () => {
-  mongoose.connect(mongoURI, () => {
-    console.log("connected to mongo successfully");
-  });
+  mongoose.connect(
+    mongoURI,
+    () => {
+      console.log("connected to mongo successfully");
+    },
+    (options = {
+      dbName: "db",
+    })
+  );
 };
 module.exports = connectToMongo;
