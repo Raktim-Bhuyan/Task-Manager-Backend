268b7a6636a0c7a7d7403f39183aa1751bd18960
Raktim Bhuyan <rokuongo@gmail.com>
2023-10-27 21:46:02 +0530

    configure mongo connection string

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const mongoose=require("mongoose");
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const mongoURI=`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.f01hh.mongodb.net/manager?authSource=admin&replicaSet=atlas-ppw553-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
+const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@taskman.grnbfob.mongodb.net/?retryWrites=true&w=majority`;
 
-const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected to mongo successfully");
-    })
-}
-module.exports=connectToMongo;
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("connected to mongo successfully");
+  });
+};
+module.exports = connectToMongo;
